19f47616b7196a46d0f9da8475656831dc81efa7
Saba Nadeem <saba21.sns@gmail.com>
2026-08-20 18:05:50 +0530

    Created a function to validate a DNA sequence.

---
+++ DNA_Toolkit.py
@@ -0,0 +1,8 @@
+Nucleotides = ["A", "C", "G", "T"]
+
+def validateSeq(dna_seq):
+    tmpseq = dna_seq.upper()
+    for nuc in tmpseq:
+        if nuc not in Nucleotides:
+            return False
+    return tmpseq
---
+++ dToolTesting.py
@@ -0,0 +1,4 @@
+from DNA_Toolkit import *
+
+rndDNAstr = "ATTgCTAT"
+print(validateSeq(rndDNAstr))
